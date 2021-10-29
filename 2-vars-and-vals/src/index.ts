18b61f25e90166a39751e3b883e0bfdaef9f63c2
Eric Laursen <ericjlaursen@gmail.com>
2021-10-29 06:42:33 -0400

    more

--- 2-vars-and-vals/src/index.ts
+++ 2-vars-and-vals/src/index.ts
@@ -7,8 +7,16 @@ let age: number = 6;
 ts will break below & compiler output will read...
   src/index.ts:7:1 - error TS2322: Type 'string' is not assignable to type 'number'.
   7 age = 'water';
+
+ts requires that variable value assignments must be the same "types".
+numbers are immutable. 
+arrays are.
 */
+
 // age = 'water';
+
+
+
 
 
 /*
