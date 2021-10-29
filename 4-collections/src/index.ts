4d64bb99d35924a00fedfb5a302cd618123603c6
Eric Laursen <ericjlaursen@gmail.com>
2021-10-29 07:11:35 -0400

    example of type-guarding

--- 4-collections/src/index.ts
+++ 4-collections/src/index.ts
@@ -10,3 +10,50 @@ let car: {
   model: 'Accord',
   year: 2017
 }
+
+/*
+  optional keys
+*/
+let carWithOptions: {
+  make: string
+  model: string
+  year: number,
+  features?: string
+} = {
+  make: 'Honda',
+  model: 'Accord',
+  year: 2017,
+  features: 'yes'
+}
+
+
+/*
+  leveraging a type-guard
+*/
+
+let carWithOptCharging: {
+  make: string
+  model: string
+  year: number,
+  chargeVoltage?: number
+} = {
+  make: 'tesla',
+  model: 'something',
+  year: 1867
+};
+
+function stringRes(obj: {
+    make: string
+    model: string
+    year: number,
+    chargeVoltage?: number
+  }): string {
+    let thisChargeVal = obj.chargeVoltage;
+    let middleManStr: string = `${obj.make} ${obj.model} ${obj.year}`;
+    // type-guarding HERE
+    if(typeof thisChargeVal !== 'undefined'){
+      middleManStr += ` ${thisChargeVal}`
+    }
+
+    return middleManStr;
+}
