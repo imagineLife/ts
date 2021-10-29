171c6923e684e5db9c4243d2a7a646fd7d723bd8
Eric Laursen <ericjlaursen@gmail.com>
2021-10-29 15:36:43 -0400

    arrs + tuples

---
+++ 5-arr/src/index.ts
@@ -0,0 +1,22 @@
+/*
+  Arrays && positional Storage
+*/
+
+// no err, no types....epic
+const strArr = ['a','b'];
+
+/*
+  Tuples
+*/
+
+let tupleArr = [1986,'October',49];
+
+/*
+  Consider the use-case:
+  - arrays are either defined-length (tuple) or random length of content
+*/
+
+// a DEFINED TUPLE TYPE
+let definedTup: [number, string] = [123,'asdf'];
+definedTup = ['qwer',234];
+// above throws err
