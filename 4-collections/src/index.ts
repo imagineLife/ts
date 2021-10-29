e0bc902044e770fe64f292f49a9b5563ae79387a
Eric Laursen <ericjlaursen@gmail.com>
2021-10-29 07:17:18 -0400

    optional keys && undefined vals

--- 4-collections/src/index.ts
+++ 4-collections/src/index.ts
@@ -57,3 +57,35 @@ function stringRes(obj: {
 
     return middleManStr;
 }
+
+/*
+  Optional Properties
+*/
+let optionalPropObj: {
+  a: number,
+  b: string,
+  c?: string
+} = {
+  a: 1,
+  b: 'asdf'
+}
+
+function needsAllFields(obj: {
+  a: number, 
+  b: string,
+  c: string | undefined
+}): boolean{
+  return true;
+}
+
+needsAllFields({a: 123, b: 'asdf'})
+/*
+  above fn returns err
+  Property 'c' is missing in type 
+    '{ a: number; b: string; }' 
+  but required in type 
+    '{ a: number; b: string; c: string; }'.
+
+  declared undefined vals are not the same as 
+    optional keys
+*/ 
